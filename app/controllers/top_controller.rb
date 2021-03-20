6dca7295c09821083a1c65d9d3a0cb3af3794bf0
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-03-20 14:05:01 +0900

    Topページ、フロント

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,7 +2,7 @@ class TopController < ApplicationController
 
   def index
     if user_signed_in?
-      @expenses = Expense.where(user_id: current_user.id).where(created_at: Time.now.all_month)
+      @expenses = Expense.where(user_id: current_user.id).where(created_at: Time.now.all_month).order('date DESC')
       time = Time.now
       @this_month = time.month
       @month_sum = @expenses.sum(:amount)
