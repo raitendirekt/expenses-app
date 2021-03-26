135339609a7cd34e48ca42c5ce47ea107db8a2c9
Raito KAGAWA <71278326+raitendirekt@users.noreply.github.com>
2021-03-26 12:07:07 +0900

    データ取得法の訂正

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,8 @@
 class FriendsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @expenses = Expense.where(created_at: Time.now.all_month)
-    expenses = Expense.where(user_id: current_user.id).where(created_at: Time.now.all_month)
+    @expenses = Expense.where(date: Time.now.all_month)
+    expenses = Expense.where(user_id: current_user.id).where(date: Time.now.all_month)
     @month_sum = expenses.sum(:amount)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @expenses = Expense.where(user_id: current_user.id).where(created_at: Time.now.all_month)
+    @expenses = Expense.where(user_id: current_user.id).where(created_at: Time.now.all_month).order('date DESC')
     @user  = User.find(params[:id])
   end
 
